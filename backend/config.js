30e3986ddbe8eda83984e96afa41b511dcb97344
Bala soundariya <balasoundariya.ravikumar@gmail.com>
2024-09-17 14:40:29 +0530

    updated db url

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT=5555;
 
-export const mongoDBURL='mongodb+srv://Bala:ramya25@cluster0.wrdqsh6.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
+export const mongoDBURL='mongodb+srv://Bala:ramya25@ecommercecluster.qeqzo.mongodb.net/books-collection'
